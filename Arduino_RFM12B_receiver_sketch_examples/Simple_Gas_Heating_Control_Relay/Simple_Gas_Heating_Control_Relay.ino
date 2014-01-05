0c57e7aaa27a272ff0b85f7696e067079f4dfcd6
glynhudson <glyn.hudson@gmail.com>
2014-01-05 01:40:25 +0000

    improve boilder control example using software RTC

--- Arduino_RFM12B_receiver_sketch_examples/Simple_Gas_Heating_Control_Relay/Simple_Gas_Heating_Control_Relay.ino
+++ Arduino_RFM12B_receiver_sketch_examples/Simple_Gas_Heating_Control_Relay/Simple_Gas_Heating_Control_Relay.ino
@@ -1,30 +1,17 @@
 // Simple relay control demo to control a central heating Boiler (In my case an old Worcester Bosch 240 with Honeywell control timer in my case)
 // Hardware: emonTx V2 with JeeLabs relay module (any other ATmega328 / Arduino + relay combo could be used) and  Raspberry Pi with RFM12Pi module to send the control RF packets
 // Software emoncms with RFM12B packet gen module: http://openenergymonitor.blogspot.co.uk/2013/11/adding-control-to-emoncms-rfm12b-packet.html
+// By Glyn Hudson - Part of the OpenEnergyMonitor.org project
+// 15th Dec 2013
 
 // For my own needs and peace of mind this code has a fail safe to turn the heating off after 1hr (I am a frugual user of heating and don't often need more than 1hr of heating at a time)
 // To protect the boiler this code also has a time delay of 5min inbetween switching to avoid the boiler being switched many times in case of system malfunction
 // Watchdog timer has been added in case of microcontroller crash (rare) 
 
-// By Glyn Hudson - Part of the OpenEnergyMonitor.org project
-// 15th Dec 2013
-// Licence: GNU GPL V3
-
-
-/*Recommended node ID allocation
-------------------------------------------------------------------------------------------------------------
--ID-        -Node Type- 
-0        - Special allocation in JeeLib RFM12 driver - reserved for OOK use
-1-4     - Control nodes 
-5-10        - Energy monitoring nodes
-11-14        --Un-assigned --
-15-16        - Base Station & logging nodes
-17-30        - Environmental sensing nodes (temperature humidity etc.)
-31        - Special allocation in JeeLib RFM12 driver - Node31 can communicate with nodes on any network group
--------------------------------------------------------------------------------------------------------------
-*/
-
 #include <JeeLib.h>	     //https://github.com/jcw/jeelib
+#include <RTClib.h>          //https://github.com/jcw/RTC
+RTC_Millis RTC;
+#include <Wire.h>
 #include <avr/wdt.h>     // Include watchdog library     
 
 typedef struct
@@ -41,26 +28,36 @@ typedef struct
 
 EmoncmsPayload emoncms;
 
-const int failsafe_off=3600000;                   //turn heating off after 1hr
-const int time_between_switching=300000;         //allow 5min inbetween switching to proect boiler 
-
-
+const int failsafe_off=(60 * 60);         //turn heating off after 1hr
+const int time_between_switching=(60*5);  //after an on/off cycle this ammount of time (5min) (in seconds) must pass before another is allowed to protect boiler
 const int HeatingRelayPin=6;
 const int ledPin=9;
-
-unsigned long timeOn;
-unsigned long timeOff=time_between_switching;    //start at 5min so we dont need to wait 5min before we can turn heating on to start with
+unsigned long timeOn, timeOff;
 boolean before, heating;
+boolean debug = 1; 
 
 
 void setup ()
 {
-
-  rf12_initialize(1,RF12_433MHZ,210);             // NodeID, Frequency, Group   - set to match RFM12Pi transmitter and your system
   
+  RTC.begin(DateTime(__DATE__, __TIME__));
+  DateTime now = RTC.now();
+  DateTime future (now.get());
+  
+  rf12_initialize(1,RF12_433MHZ,210); // NodeID, Frequency, Group
+  Serial.begin(9600);
+  
+  if (Serial) debug = 1; else debug=0;  
+  if (debug ==1) Serial.println("heating boiler relay controller - openenergymonitor.org");
+  if (debug==0) Serial.end();
+
   pinMode(HeatingRelayPin, OUTPUT);
   pinMode(ledPin, OUTPUT);
   digitalWrite(HeatingRelayPin, LOW);
+
+  digitalWrite(ledPin, HIGH);
+  delay(1500);
+  digitalWrite(ledPin, HIGH);
   
   wdt_enable(WDTO_8S);   // Enable hardware anti crash watchdog: max 8 seconds
 }
@@ -76,25 +73,39 @@ void loop ()
       // The packet data is contained in rf12_data, the *(EmoncmsPayload*) part tells the compiler
       // what the format of the data is so that it can be copied correctly
       emoncms = *(EmoncmsPayload*) rf12_data;
-
-      if ((emoncms.heating==1) && (before==0) && ((millis()-timeOff) > time_between_switching))        //look for step change from 0 to 1 and only alow heating to turn on after it's been off for 5min to save wear on boiler if something was to go wrong
+      
+      RTC.adjust(DateTime(2013, 1, 1, rf12_data[1], rf12_data[2], rf12_data[3]));   //set software RTC based on time received from emoncms
+      DateTime now = RTC.now();
+      
+      if (debug==1)
+      {
+        Serial.print(emoncms.blankchar); Serial.print(" "); Serial.print(emoncms.hour); Serial.print(" "); Serial.print(emoncms.minute); Serial.print(" ");
+        Serial.print(emoncms.second); Serial.print(" "); Serial.print(emoncms.target_temperature); Serial.print(" "); Serial.print(emoncms.hysteresis); Serial.print(" ");
+        Serial.println(emoncms.heating);
+        Serial.print(now.hour(), DEC); Serial.print(':'); Serial.print(now.minute(), DEC); Serial.print(':'); Serial.print(':'); Serial.print(now.second(), DEC); Serial.println();
+        //Serial.print(before); Serial.print(" "); Serial.println(timeOn); Serial.println(" "); Serial.println(timeOff); Serial.println(" "); Serial.println(millis()); Serial.println(" ");
+        Serial.print("Heating: "); Serial.println(heating);
+        Serial.println();
+       }
+     
+      
+      if ((emoncms.heating==1) && (((now.get() - timeOff) > time_between_switching)))        //look for step change from 0 to 1 and only alow heating to turn on after it's been off for 5min to save wear on boiler if something was to go wrong
       {
         heating=1;
-        timeOn=millis();
-        before=1;
+        timeOn = now.get();                      //record when the heading was turned on in seconds since 2000
       }
       
-      if ((emoncms.heating==0) && ((millis()-timeOn) > time_between_switching ))   //only alow heating to turn off after it's been on for 5min to save wear on boiler if something was to go wrong
+      if (emoncms.heating==0)
       {
+        
+        if (heating==1) timeOff = now.get();     //if heatiing was on before record the time it was turned off in seconds since 2000
         heating=0;
-        before=0;
-        timeOff=millis();
      }
   }
   }
 
-
-if ((millis()-timeOn) > failsafe_off) heating=0;          //if heating has been on for 1hr turn it off
+DateTime now = RTC.now();
+if ((now.get() - timeOn) > failsafe_off) heating=0;          //if heating has been on for 1hr turn it off
 
 
 if (heating==0) //turn the heating off 
